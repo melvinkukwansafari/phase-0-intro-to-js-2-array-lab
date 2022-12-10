4879ba86e047b195393dc609c326b6ddf895831f
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2022-12-10 14:34:43 +0300

    still finding solutions

--- index.js
+++ index.js
@@ -32,14 +32,40 @@ function destructivelyPrependCat(name) {
     // Check if the array is empty
     if (!window.cats) {
       // If the array is empty, there is nothing to remove, so return
-      windows.cats = [];
+      window.cats = [];
     }
   
     // Use the shift() method to remove the first element from the array
-    windows.cats.shift(name);
+    window.cats.shift(name);
   
     // Return the removed element
   }
+  function appendCat(name) {
+    // Create a new array by copying the existing cats array
+    var newCats = cats.slice();
+    // Use the push method to add the new cat to the new array
+    newCats.push(name);
+    // Return the new array
+    return newCats;
+  }
+  function prependCat(name,) {
+    // Create a new array by copying the elements of the original array
+    let newCats = [...cats];
+  
+    // Add the new cat to the beginning of the new array
+    newCats.unshift(name);
+  
+    // Return the new array
+    return newCats;
+  }
+  function removeLastCat(cats) {
+    let newCats = cats.slice(cats); // create a copy of the original array
+    newCats.splice(-1, 1); // remove the last element
+    return newCats;
+  }
+  
+  
+  
   
   
   
